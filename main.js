0f657746a29ec3402ef428fa286e91434d313814
mahdihajati <you@example.com>
2022-01-20 00:38:26 +0330

    just give data

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log("hello world")
+let info = {
+
+    firstname: 'mahdi' ,
+    lastname: 'hajati',
+    age: 19,
+    haircollor: 'black',
+    user: 'madix',
+    isalive: true,
+
+}
+console.log(info)
